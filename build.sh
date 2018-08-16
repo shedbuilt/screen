a64ba743bcf5b9c837f3b0cebc93eb449027051a
Auston Stewart <auston@shedbuilt.net>
2018-08-16 10:33:39 -1000

    Updating to scren 4.6.2

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+# Configure
 ./configure --prefix=/usr                     \
             --infodir=/usr/share/info         \
             --mandir=/usr/share/man           \
             --with-socket-dir=/run/screen     \
             --with-pty-group=5                \
-            --with-sys-screenrc=/etc/screenrc
-sed -i -e "s%/usr/local/etc/screenrc%/etc/screenrc%" {etc,doc}/*
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
-mkdir -v ${SHED_FAKE_ROOT}/etc
-install -m 644 etc/etcscreenrc ${SHED_FAKE_ROOT}/etc/screenrc.default
+            --with-sys-screenrc=/etc/screenrc &&
+# Patch
+sed -i -e "s%/usr/local/etc/screenrc%/etc/screenrc%" {etc,doc}/* &&
+# Build & Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Install defaults
+install -v -dm755 "${SHED_FAKE_ROOT}${SHED_PKG_DEFAULTS_INSTALL_DIR}/etc" &&
+install -v -m644 etc/etcscreenrc "${SHED_FAKE_ROOT}${SHED_PKG_DEFAULTS_INSTALL_DIR}/etc/screenrc"
